c5f2fccb786d7a9baf7846727e824d3224425ab6
jlee975 <jonathan.lee.975@gmail.com>
2015-07-20 08:59:50 -0500

    Update file.h

--- pbl/fs/file.h
+++ pbl/fs/file.h
@@ -1,89 +1,92 @@
-/* Copyright (c) 2014, Pollard Banknote Limited
-   All rights reserved.
-
-   Redistribution and use in source and binary forms, with or without modification,
-   are permitted provided that the following conditions are met:
-
-   1. Redistributions of source code must retain the above copyright notice, this
-   list of conditions and the following disclaimer.
-
-   2. Redistributions in binary form must reproduce the above copyright notice,
-   this list of conditions and the following disclaimer in the documentation and/or
-   other materials provided with the distribution.
-
-   3. Neither the name of the copyright holder nor the names of its contributors
-   may be used to endorse or promote products derived from this software without
-   specific prior written permission.
-
-   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-   ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-   WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-   DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-   FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-   DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-   SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-   CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-   OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-   OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-#ifndef FILEUTILS_H
-#define FILEUTILS_H
+#ifndef FILE_H
+#define FILE_H
 
 #include <string>
+
+#if !defined( _WIN32 ) && ( defined( __unix__ ) || defined( __unix ) || ( defined( __APPLE__ ) && defined( __MACH__ )))
+#include <unistd.h>
+#if defined( _POSIX_VERSION )
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#endif
+#endif
+
+#include "perms.h"
 
 namespace pbl
 {
 namespace fs
 {
-/** @brief Get an absolute path of a file
- * @param filepath The path of an (existing) file
- * @returns An absolute path that corresponds to the same file as filepath, or
- *   an empty string if there is an error.
- */
-std::string absolute_path(const std::string& filepath);
 
-/** @brief Copy the file at source to dest
- * @param source A file to copy
- * @param dest A file (including name) of the file to create
- * @returns true iff dest exists and is a copy of source
- *
- * If source does not exist or is not a file, the copy will fail.
- *
- * If dest exists, it will be overwritten. Dest will have the same file
- * permissions of source, if possible (subject to umask).
- *
- * This function copies the source file "safely". That is, in the event of an
- * error, dest is unaltered (or, if it didn't exist, continues to not exist).
- */
-bool copy_file(const std::string& source, const std::string& dest);
+class file
+{
+public:
+	enum flags_t
+	{
+		create    = O_CREAT | O_EXCL,
+		readonly  = O_RDONLY,
+		writeonly = O_WRONLY,
+		readwrite = O_RDWR
+	};
 
-/** @brief Return the name of the file system component indicated by path
- * @param path A file path
- * @returns The name of the directory or file that the path points to, or the
- * empty string if there's an error
- *
- * The exact behavior of this function is platform dependent, because paths are.
- * However, whatever the platform, it should return the name of the file or
- * directory pathed. Ex., on POSIX platforms it should return "readme.txt" for
- * "/home/user/documents/readme.txt"; similarly it should return "readme.txt"
- * for "C:\Windows\readme.txt" on Windows platforms.
- *
- * This function does not access the file system. It merely parses the string.
- * In particular, it does not check if the path is valid and/or accessible. It
- * also does not check that the component before "." or ".." is, in fact, a
- * directory.
- *
- * If the last component cannot be determined for whatever reason, the string is
- * considered malformed and the empty string is returned. Some examples of
- * a "malformed" string are "", ".", "..", "usr/..". (However, something like
- * "usr/lib/.." should return "usr".)
- */
-std::string basename(const std::string& path);
+	typedef ssize_t size_type;
 
-std::string dirname(const std::string& path);
+	file();
+	file(const std::string& name, int flags);
+	file(const std::string& name, int flags, pbl::fs::perms);
+	~file();
 
-std::string cleanpath(const std::string& path);
+	bool is_open() const;
+
+	/// @bug if symlink, then is the link end a file
+	bool is_file() const;
+
+	size_type read(char* buffer, std::size_t n);
+
+	size_type write(const char* buffer, std::size_t n);
+
+	pbl::fs::perms permissions() const;
+
+	void flush();
+
+	void remove();
+
+	void chmod(pbl::fs::perms m);
+
+	void chmod(const file&);
+
+	/// @todo Decide on precise behavior and how "name" is interpreted. Ex.,
+	/// temp file should be made in same directory as name
+	bool mkstemp(const std::string& name);
+
+	bool rename(const std::string& dest);
+
+	/// Make a temporary file "real" (by renaming it)
+	bool realize(const std::string& dest);
+
+	// append contents of in. Both file descriptors must be valid
+	bool copy(file& in);
+
+	size_type size() const;
+
+	// 0 - files are not the same, 1 files are the same
+	// -1 some kind of error happened
+	int compare(const file&);
+private:
+	class seek_guard;
+
+	file(const file&);
+	file& operator=(const file&);
+	bool get_stat() const;
+
+	std::string filename;
+
+	int                  fd;
+	bool                 is_temp;
+	mutable struct stat* filestat;
+};
 }
 }
 
-#endif // FILEUTILS_H
+#endif // FILE_H
