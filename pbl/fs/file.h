f057f09d9fc7c2e7af00ab1b23622ef1ad99b954
jlee975 <jonathan.lee.975@gmail.com>
2015-07-20 08:53:53 -0500

    Update file.h

--- pbl/fs/file.h
+++ pbl/fs/file.h
@@ -3,9 +3,9 @@
 
 #include <string>
 
-#if !defined(_WIN32) && (defined(__unix__) || defined(__unix) || (defined(__APPLE__) && defined(__MACH__)))
+#if !defined( _WIN32 ) && ( defined( __unix__ ) || defined( __unix ) || ( defined( __APPLE__ ) && defined( __MACH__ )))
 #include <unistd.h>
-#if defined(_POSIX_VERSION)
+#if defined( _POSIX_VERSION )
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
@@ -22,60 +22,69 @@ namespace fs
 class file
 {
 public:
-    enum flags_t {
-        create = O_CREAT | O_EXCL,
-        readonly = O_RDONLY,
-        writeonly = O_WRONLY,
-        readwrite = O_RDWR
-    };
+	enum flags_t
+	{
+		create    = O_CREAT | O_EXCL,
+		readonly  = O_RDONLY,
+		writeonly = O_WRONLY,
+		readwrite = O_RDWR
+	};
 
-    typedef ssize_t size_type;
+	typedef ssize_t size_type;
 
-    file();
-    file(const std::string& name, int flags);
-    file(const std::string& name, int flags, pbl::fs::perms);
-    ~file();
+	file();
+	file(const std::string& name, int flags);
+	file(const std::string& name, int flags, pbl::fs::perms);
+	~file();
 
-    bool is_open() const;
+	bool is_open() const;
 
-    /// @bug if symlink, then is the link end a file
-    bool is_file() const;
+	/// @bug if symlink, then is the link end a file
+	bool is_file() const;
 
-    size_type read(char* buffer, std::size_t n);
+	size_type read(char* buffer, std::size_t n);
 
-    size_type write(const char* buffer, std::size_t n);
+	size_type write(const char* buffer, std::size_t n);
 
-    pbl::fs::perms permissions() const;
+	pbl::fs::perms permissions() const;
 
-    void flush();
+	void flush();
 
-    void remove();
+	void remove();
 
-    void chmod(pbl::fs::perms m);
+	void chmod(pbl::fs::perms m);
 
-    void chmod(const file&);
+	void chmod(const file&);
 
-    /// @todo Decide on precise behavior and how "name" is interpreted. Ex.,
-    /// temp file should be made in same directory as name
-    bool mkstemp(const std::string& name);
+	/// @todo Decide on precise behavior and how "name" is interpreted. Ex.,
+	/// temp file should be made in same directory as name
+	bool mkstemp(const std::string& name);
 
-    bool rename(const std::string& dest);
+	bool rename(const std::string& dest);
 
-    /// Make a temporary file "real" (by renaming it)
-    bool realize(const std::string& dest);
+	/// Make a temporary file "real" (by renaming it)
+	bool realize(const std::string& dest);
 
-    // append contents of in. Both file descriptors must be valid
-    bool copy(file& in);
+	// append contents of in. Both file descriptors must be valid
+	bool copy(file& in);
+
+	size_type size() const;
+
+	// 0 - files are not the same, 1 files are the same
+	// -1 some kind of error happened
+	int compare(const file&);
 private:
-    file(const file&);
-    file& operator=(const file&);
-    bool get_stat() const;
+	class seek_guard;
 
-    std::string filename;
+	file(const file&);
+	file& operator=(const file&);
+	bool get_stat() const;
 
-    int fd;
-    bool is_temp;
-    mutable struct stat* filestat;
+	std::string filename;
+
+	int                  fd;
+	bool                 is_temp;
+	mutable struct stat* filestat;
 };
 }
 }
